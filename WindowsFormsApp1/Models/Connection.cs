8f8e6eee2a6766f4e49088bc0c6f02e00d987d03
Dawid Jedrzejczak <dawid.jedrzejczak@gmail.com>
2023-01-15 20:29:47 +0100

    asdasd

--- WindowsFormsApp1/Models/Connection.cs
+++ WindowsFormsApp1/Models/Connection.cs
@@ -9,9 +9,24 @@ namespace WindowsFormsApp1.Models
 {
     public class  Connection
     {
-        
 
-        
+        private string connectionString;
+        private SqlConnection connection;
+
+        private Connection()
+        {
+            connection = new SqlConnection();
+        }
+        public void Connect()
+        {
+            connection = new SqlConnection(GetConnectionString());
+            connection.Open();
+        }
+        private string GetConnectionString()
+        {
+            return "Server=DESKTOP-MS2A1CD;Database=Firma_Transportowa;Trusted_Connection=true";
+        }
+
 
     }
 }
